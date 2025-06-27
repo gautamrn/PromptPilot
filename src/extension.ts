fbdb3c4bfcfd16be030248a5580754b701c3b9c0
gautamrn <gautamrn@umich.edu>
2025-06-26 21:29:32 -0400

    everything but playground working

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,14 @@ import { registerGenerateStepsCommand } from './commands/generateSteps';
 import { registerExplainCodeCommand } from './commands/explainCode';
 import { registerSuggestRefactorCommand } from './commands/suggestRefactor';
 import { registerSuggestCommitMessageCommand } from './commands/suggestCommitMessage';
+import { registerPlaygroundPanelCommand } from './commands/playground';
 
 export function activate(context: vscode.ExtensionContext) {
   registerGenerateStepsCommand(context);
   registerExplainCodeCommand(context);
   registerSuggestRefactorCommand(context);
   registerSuggestCommitMessageCommand(context);
+  registerPlaygroundPanelCommand(context);
 }
 
 export function deactivate() {}
